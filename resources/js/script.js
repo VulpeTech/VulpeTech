1f44f1ce45327aea2c3594fcc4798d0ac9a2e331
Matheus Carvalho <matheus_carvalho492@hotmail.com>
2023-09-14 18:32:45 -0300

    Delete resources directory

--- resources/js/script.js
+++
@@ -1,8 +0,0 @@
-const menuDiv = document.querySelector(".menu")
-const sideBar = document.querySelector(".sidebar")
-
-menuDiv.addEventListener("click", () => {
-    menuDiv.classList.toggle("active")
-    sideBar.classList.toggle("show")
-    
-})
